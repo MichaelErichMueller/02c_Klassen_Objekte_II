1104192c4e1d3c096a2ccc68cd23bf9093ae7c09
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-24 11:15:47 +0100

    Update

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,45 +5,68 @@ public class Cat {
    private String name;
    private String furColor;
    private int age;
-
-    public Cat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-	}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFurColor() {
-        return furColor;
-    }
-
-    public void setFurColor(String furColor) {
-        this.furColor = furColor;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+   private boolean isFemale;
 
 
-
-    
-
-
-
-    
-
-
-    
+    public Cat(String name, String furColor, int age, boolean isFemale) {
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+    this.isFemale = isFemale;
 }
+    public String getStringAttributes(String flag) {
+        
+        switch (flag) {
+            case "#name":
+                return name;
+                
+
+            case "color":
+                return furColor;
+                
+                
+            default:
+                return "ERROR";
+                
+        }
+    }
+        public String getAge() {
+            if (!isFemale) {
+                return Integer. toString(age);
+            } else {
+                return checkEscalationLevel();
+            }
+            
+        }
+        
+        public String checkEscalationLevel(){
+            switch (2) {
+                case 1:
+                    
+                    return"This is not Ok!";
+                    case 2:
+                    
+                    return" Ive told you !";
+                    
+                    case 3:
+                    
+                    return"Talk to the Hand";
+                
+                default:
+                    return"1'*?§%";
+            }
+
+        }
+    }
+
+
+
+    
+
+
+
+    
+
+
+    
+
